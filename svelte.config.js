a568b5f1775ad1ed47ab1f8c5022ed1ae94f1243
cirroskais <cirroskais@madhouselabs.net>
2025-02-10 21:52:40 -0500

    DOCKEr

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,15 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
-    kit: { adapter: adapter() },
+    kit: {
+        adapter: adapter({
+            precompress: {
+                brotli: true,
+                gzip: true,
+                files: ["html", "js", "json", "css", "svg", "xml", "wasm", "png", "gif"],
+            },
+        }),
+    },
 };
 
 export default config;
